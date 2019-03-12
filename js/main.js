75031cd045ab9c4ee4dc90519a7a6ce0d7a39483
Vando Araujo <tecladista.vando@gmail.com>
2019-03-12 00:41:23 -0300

    adicao pagina celulas

--- js/main.js
+++ js/main.js
@@ -320,15 +320,15 @@
 		// + minutes + "Minutes " + seconds + "Seconds ";
 
 		// Display the result in an element with id="demo"
-		document.getElementById("days").innerHTML = days +" <small>days</small>";
-		document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
-		document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
-		document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
+		document.getElementById("days").innerHTML = days +" <small>dias</small>";
+		document.getElementById("hours").innerHTML = hours + " <small>horas</small> ";
+		document.getElementById("minutes").innerHTML = minutes + " <small>minutos</small> ";
+		document.getElementById("seconds").innerHTML = seconds + " <small>segundos</small> ";
 
 		// If the count down is finished, write some text 
 		if (distance < 0) {
 		 clearInterval(x);
-		 document.getElementById("demo").innerHTML = "The Wedding Ceremony is Over";
+		 document.getElementById("demo").innerHTML = "O ano terminou";
 		}
 		}, 1000);
 
