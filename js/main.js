282d32c9b7d917b0620866e190c4e2763ea4c96d
Vando Ribeiro de Araujo <vando.araujo@serpro.gov.br>
2020-07-09 00:15:49 -0300

    ebf

--- js/main.js
+++ js/main.js
@@ -63,27 +63,22 @@
    $.Scrollax();
 
 	var carousel = function() {
-		$('.carousel-testimony').owlCarousel({
-			center: true,
-			loop: true,
-			items:1,
-			margin: 30,
-			stagePadding: 0,
-			nav: true,
-			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+		$('.owl-carousel').owlCarousel({
+			loop:true,
+			margin:10,
+			nav:true,
 			responsive:{
 				0:{
-					items: 1
+					items:1
 				},
 				600:{
-					items: 2
+					items:3
 				},
 				1000:{
-					items: 3
+					items:5
 				}
 			}
 		});
-
 	};
 	carousel();
 
